6528601c6713d7baaaf3ca2187242277060f30b8
machukhinktato <tarabrinmv@gmail.com>
2020-08-05 22:26:11 +0300

    fixes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-num, b, c, count = int(input('Enter a number')), '  ', '   ', 0
+num, b, c, count = int(input('Enter a number: ')), '  ', '   ', 0
 for i in range(0, num+1):
     if i > 9:
         b = b + str(i) + '   '
@@ -6,13 +6,16 @@ for i in range(0, num+1):
     else:
         b = b + str(i) + '    '
         c = c + str(i) + ' |'
-    for a in range(1, num+1):
+    for a in range(0, num+1):
         d = str(a*i)
         for letter in d:
             count += 1
         c = c + str(a*i) + ' '*(5-count)
         count = 0
     c = c + '\n   '
+
 print((' ')*4 + b)
 print((' ')*6 + ('-'+ '    ')*num)
 print(c)
+
+# something interesting
